9f72300ec1e61abc1190c6df8524d0d902d06bb9
raphaeldemoura <raphaeldemoura3715@gmail.com>
2025-05-26 22:19:47 -0300

    feat:introdução do react

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Body.css'
+
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Macacos gostam de banana🍌</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur totam fugiat molestias, ut quos est, debitis eveniet suscipit aut distinctio voluptatibus pariatur provident dolorem corporis labore expedita nostrum accusantium tempore!</p>
+        </div>
+  )
+}
+
+export default Body
