f9dd4add3379abdfdedcc19463a8640b2bcc971a
raphaeldemoura <raphaeldemoura3715@gmail.com>
2025-05-28 19:49:07 -0300

    feat:exercicios de calculos

--- ola/src/components/Header.jsx
+++ ola/src/components/Header.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
+  const [searchTerm , setSearchchTerm] = React.useState('')
   return (
     <div className='Header'> 
-        <h1>Macaco🐒🙉🐵</h1>
+        <h1>Buscador de Personagens - Harry Poter</h1>
+
+        <input type="text" 
+        placeholder='Digite o nome...'
+        value={searchTerm}
+        onChange={(e) => setSearchchTerm(e.target.value)}
+        className='search'
+        />
     </div>
   )
 }
