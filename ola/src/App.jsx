23e3acc26c191791f4272d41d1be64f57c26f8a5
raphaeldemoura <raphaeldemoura3715@gmail.com>
2025-05-28 21:55:21 -0300

    feat:buscador de nomes harry poter

--- ola/src/App.jsx
+++ ola/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Body from './components/Body'
 import Header from './components/Header'
 
 function App() {
-  
+  const [characters, setCharacters] = useState([])
+  useEffect(() =>{
+    fetch('https://hp-api.onrender.com/api/characters')
+    .then((res) => res.json())
+    .then(setCharacters)
+    .catch((err) => console.error('Erro ns API', err))
+
+  },{})
+
   return (
     <div className='App'>
     <Header />
-    <Body />
+    <Body characters={characters}/>
 
     </div>
   )
--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -2,10 +2,30 @@ import { useState } from 'react'
 import './Body.css'
 
 
-function Body() {
+function Body(props) {
+  console.log(props.characters);
+
   return (
-    <div className='Body'>  
-    
+    <div className='Body'>
+      <div className='grid'>
+        {
+          props.characters.map((character) => (
+            <div className='card'>
+              {character.image ? (
+                <img src={character.image} alt={character.name} />
+              ) :
+                (
+                  <div className='noImage'>Imagem indisponivel</div>
+                )
+              }
+              <h2>{character.name}</h2>
+              <p>
+                <strong>Casa:</strong>{character.house || "Desconhecida"}
+              </p>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
